2fa118161ef5f0de8c40764139580f9d7d1908e5
Tom Darci <tom.darci@opower.com>
2012-11-06 16:24:59 -0800

    wookie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blergh::Application.config.secret_token = '3aa2aeff91bd24cea07e63ad3557bb7a108a76e137cc5ca4c9d3578f22728ceed2bacd155329bbcdc3ca5ea08d205891a6221a2123207e91baca17ec808c4c2d'
